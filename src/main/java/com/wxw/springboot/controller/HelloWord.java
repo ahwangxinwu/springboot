63c85f092102ce5d662ba88db633755d5d0247d8
wangxinwu <540548207@qq.com>
2018-09-26 16:13:29 +0800

    提交测试

--- src/main/java/com/wxw/springboot/controller/HelloWord.java
+++ src/main/java/com/wxw/springboot/controller/HelloWord.java
@@ -11,23 +11,4 @@ public class HelloWord {
         return "helloword";
     }
 
-    @RequestMapping("/hello2")
-    public String hello2(){
-        return "热部署测试";
-    }
-
-    @RequestMapping("/hello3")
-    public String hello3(){
-        return "热部署测试3";
-    }
-
-    @RequestMapping("/hello4")
-    public String hello4(){
-        return "热部署测试4";
-    }
-
-    @RequestMapping("/hello5")
-    public String hello5(){
-        return "热部署测试5";
-    }
 }
