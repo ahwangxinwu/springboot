6d9b12505a4530c0cabb1529b8905bc9f087c5a6
wangxinwu <540548207@qq.com>
2018-09-27 11:09:28 +0800

    递归算法

--- src/main/java/com/wxw/springboot/controller/HelloWord.java
+++ src/main/java/com/wxw/springboot/controller/HelloWord.java
@@ -11,4 +11,23 @@ public class HelloWord {
         return "helloword";
     }
 
+
+    public static void main(String[] args) {
+        System.out.println("结果是："+HelloWord.foo(30));
+    }
+
+    /**
+     * 递归算法实现
+     */
+    public static int foo(int i){
+        if(i<=0){
+            return 0;
+        }
+        else if(i>0 && i<=2){
+            return 1;
+        }
+        return foo(i-1) + foo(i-2);
+    }
+
+
 }
